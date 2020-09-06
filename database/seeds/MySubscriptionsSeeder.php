6e6c2581eda598a45ad573c766ea2722033cc4ff
Naoto <naoto>
2020-09-07 00:49:20 +0900

    Landing Pageの作成

--- database/seeds/MySubscriptionsSeeder.php
+++ database/seeds/MySubscriptionsSeeder.php
@@ -45,15 +45,25 @@ class MySubscriptionsSeeder extends Seeder
             ]
         );
 
-        //https://mail.google.com/mail/u/0/#search/phpstorm/FMfcgxwBTsXbGXPFpPnxtTsBrfTXKnfl
         Subscription::create(
             [
                 'user_id' => $user->id,
-                'service_id' => 1,
-                'name' => "PhpStorm",
+                'service_id' => 3,
+                'name' => "Spotify",
                 'cycle_id' => 2,
-                'price' => 13700,
-                'first_bill' => "2020-12-29"
+                'price' => 980,
+                'first_bill' => "2019-06-21"
+            ]
+        );
+
+        Subscription::create(
+            [
+                'user_id' => $user->id,
+                'service_id' => 7,
+                'name' => "iCloud",
+                'cycle_id' => 1,
+                'price' => 130,
+                'first_bill' => "2017-02-11"
             ]
         );
 
@@ -61,22 +71,35 @@ class MySubscriptionsSeeder extends Seeder
             [
                 'user_id' => $user->id,
                 'service_id' => 1,
-                'name' => "1Password",
-                'cycle_id' => 2,
-                'price' => 3900,
-                'first_bill' => "2020-07-27"
+                'name' => "Adobe",
+                'cycle_id' => 1,
+                'price' => 5680,
+                'first_bill' => "2018-11-24"
             ]
         );
 
-        Subscription::create(
-            [
-                'user_id' => $user->id,
-                'service_id' => 1,
-                'name' => "1Password",
-                'cycle_id' => 2,
-                'price' => 3900,
-                'first_bill' => "2020-07-27"
-            ]
-        );
+
+//        //https://mail.google.com/mail/u/0/#search/phpstorm/FMfcgxwBTsXbGXPFpPnxtTsBrfTXKnfl
+//        Subscription::create(
+//            [
+//                'user_id' => $user->id,
+//                'service_id' => 1,
+//                'name' => "PhpStorm",
+//                'cycle_id' => 2,
+//                'price' => 13700,
+//                'first_bill' => "2020-12-29"
+//            ]
+//        );
+//
+//        Subscription::create(
+//            [
+//                'user_id' => $user->id,
+//                'service_id' => 1,
+//                'name' => "1Password",
+//                'cycle_id' => 2,
+//                'price' => 3900,
+//                'first_bill' => "2020-07-27"
+//            ]
+//        );
     }
 }
