453f4e91d6589a01dee8e64afe137d6928e07c41
Naoto <naoto>
2020-09-23 01:57:03 +0900

    fix welcom

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -132,7 +132,7 @@
                                 <div class="mt-5">
                                     <h5 class="text-lg leading-6 font-medium text-gray-900">コミュニティ</h5>
                                     <p class="mt-2 text-base leading-6 text-gray-500">
-                                        すべてのユーザーのサブスクが一覧として表示され、他人のサブスクを見ることができます。
+                                        他のユーザーのサブスクが一覧として表示され、他人のサブスクを見ることができます。
                                     </p>
                                 </div>
                             </div>
@@ -144,16 +144,32 @@
                                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                                          xmlns="http://www.w3.org/2000/svg">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                              d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
+                                              d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
                                     </svg>
                                 </div>
                                 <div class="mt-5">
-                                    <h5 class="text-lg leading-6 font-medium text-gray-900">プライベートモード</h5>
+                                    <h5 class="text-lg leading-6 font-medium text-gray-900">もしかして契約している?</h5>
                                     <p class="mt-2 text-base leading-6 text-gray-500">
-                                        登録しているサブスクを自分だけしか見れないようにします。
+                                        Gmailと連携して契約されているであろうサブスクの一覧を取得して漏れがないようにします。
                                     </p>
                                 </div>
                             </div>
+                            {{--                            <div>--}}
+                            {{--                                <div--}}
+                            {{--                                    class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">--}}
+                            {{--                                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"--}}
+                            {{--                                         xmlns="http://www.w3.org/2000/svg">--}}
+                            {{--                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"--}}
+                            {{--                                              d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>--}}
+                            {{--                                    </svg>--}}
+                            {{--                                </div>--}}
+                            {{--                                <div class="mt-5">--}}
+                            {{--                                    <h5 class="text-lg leading-6 font-medium text-gray-900">プライベートモード</h5>--}}
+                            {{--                                    <p class="mt-2 text-base leading-6 text-gray-500">--}}
+                            {{--                                        登録しているサブスクを自分だけしか見れないようにします。--}}
+                            {{--                                    </p>--}}
+                            {{--                                </div>--}}
+                            {{--                            </div>--}}
                             <div class="mt-10 lg:mt-0">
                                 <div
                                     class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
