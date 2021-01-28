300ca3817a0cbb7ed38a8fdedd48aea661d09e38
Naoto <naoto>
2021-01-28 23:08:12 +0900

    fix

--- resources/views/subscriptions/index.blade.php
+++ resources/views/subscriptions/index.blade.php
@@ -64,7 +64,7 @@
     <div class="w-1/5 mx-auto mt-10">
         <p class="text-gray-600 mb-2 text-center">\snsにシェアしよう/</p>
         <span class="w-full inline-flex rounded-md shadow-sm">
-          <a href="https://twitter.com/share?text={{$tweet_text}}&hashtags=サブスク"
+          <a href="https://twitter.com/share?text={{$tweet_text}}"
              target="_blank"
              class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition duration-150 ease-in-out"
              aria-label="Sign in with Twitter">
