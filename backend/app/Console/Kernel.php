15e4ff2337d4288fe4234fb27ea545cf90ea8d7d
Naoto <naoto>
2021-03-21 12:09:54 +0900

    test

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('mail:subscription:report')->daily();
-        $schedule->command('tweet:report')->daily();
+        $schedule->command('tweet:report')->everyMinute();
     }
 
     /**
